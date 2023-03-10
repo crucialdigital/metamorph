eebcc5b064df04a7a23403439d53f91ec2ad704b
Humbert <djak1223@gmail.com>
2023-03-10 22:14:38 +0000

    fix bug

--- routes/metamorph.php
+++ routes/metamorph.php
@@ -8,7 +8,7 @@ use CrucialDigital\Metamorph\Http\Controllers\MasterCrudController;
 use CrucialDigital\Metamorph\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/' . trim(config('route_prefix'), "/ \t\n\r\0\x0B"))
+Route::prefix('api/' . trim(config('route_prefix')))
     ->middleware(['api'])->group(function () {
     Route::middleware(config('metamorph.middlewares'))->group(function () {
         Route::post('/search/{entity}', [SearchController::class, 'search']);
