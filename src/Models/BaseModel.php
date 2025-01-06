88c64d76de3cdd28b2751fcaacb59714580f2947
humbert <djak1223@gail.com>
2025-01-06 10:59:53 +0000

    fix: bugs

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -30,9 +30,6 @@ abstract class BaseModel extends Model
     public $timestamps = true;
     protected $guarded = ['_id'];
     protected $appends = ['id'];
-
-    protected $primaryKey = '_id';
-
     protected $casts = [
         'created_at' => 'datetime',
         'update_at' => 'datetime',
